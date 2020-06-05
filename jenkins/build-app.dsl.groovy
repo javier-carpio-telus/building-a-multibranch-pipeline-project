10448f02210c0f1f1c7eec1b961c7a1eb1e79684
javier-carpio-telus <javier.carpio@telus.com>
2020-06-05 01:47:26 -0600

    Added first dsl

--- jenkins/build-app.dsl.groovy
+++ jenkins/build-app.dsl.groovy
@@ -1,10 +1,21 @@
 def gitUrl = 'git://github.com/test/test'
 
-job(${params.NEW_JOB_NAME}) {
-    scm {
-        git(gitUrl)
-    }
-    steps {
-        sh 'echo "Job created from seed!"'
+//job(${params.NEW_JOB_NAME}) {
+//    scm {
+//        git(gitUrl)
+//    }
+//    steps {
+//        sh 'echo "Job created from seed!"'
+//    }
+//}
+
+
+pipeline {
+    stages {
+        stage('Build') { 
+            steps {
+                sh 'echo "Job created from seed!"' 
+            }
+        }
     }
 }
