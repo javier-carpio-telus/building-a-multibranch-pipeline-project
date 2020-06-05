3fa3ba047af765651a4db535e98e82183cd1a3e1
javier-carpio-telus <javier.carpio@telus.com>
2020-06-05 02:43:48 -0600

    renamed dsl file

--- jenkins/build_app.dsl.groovy
+++ jenkins/build_app.dsl.groovy
@@ -10,13 +10,19 @@ def gitUrl = 'git://github.com/test/test'
 //}
 
 
-pipeline ("my_new_pipeline") {
-    agent any
-    stages {
-        stage('Build') { 
-            steps {
-                sh 'echo "Job created from seed!"' 
-            }
-        }
-    }
+//pipeline ("my_new_pipeline") {
+//    agent any
+//    stages {
+//        stage('Build') { 
+//            steps {
+//                sh 'echo "Job created from seed!"' 
+//            }
+//        }
+//    }
+//}
+
+job('my-new-seeded-job') {
+  steps {
+    sh('echo Hello World!')
+  }
 }
